6c05d643fe9cd52e905607d1a7570fc39a8da950
Dominik Lehrl <d.lehrl@remind.de>
2024-10-30 14:37:53 +0100

    fix: remove unused class imports

--- Classes/Domain/Finishers/Newsletter2goSubscribeFinisher.php
+++ Classes/Domain/Finishers/Newsletter2goSubscribeFinisher.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Remind\Newsletter2go\Domain\Finishers;
 
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder;
 use TYPO3\CMS\Form\Domain\Finishers\AbstractFinisher;
 
 class Newsletter2goSubscribeFinisher extends AbstractFinisher
