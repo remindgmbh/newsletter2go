97f2e50d17f92d4f7233b83837d5716a218a17ce
Dominik Lehrl <d.lehrl@remind.de>
2024-10-31 16:57:16 +0100

    feat: provide subscribe finisher logic

--- Classes/Domain/Finishers/Newsletter2goSubscribeFinisher.php
+++ Classes/Domain/Finishers/Newsletter2goSubscribeFinisher.php
@@ -4,13 +4,81 @@ declare(strict_types=1);
 
 namespace Remind\Newsletter2go\Domain\Finishers;
 
+use Exception;
+use GuzzleHttp\RequestOptions;
+use TYPO3\CMS\Core\Http\RequestFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Form\Domain\Finishers\AbstractFinisher;
 
 class Newsletter2goSubscribeFinisher extends AbstractFinisher
 {
+    const NL2GO_SUBMIT_URL = 'https://api.newsletter2go.com/forms/submit/';
+
+    private RequestFactory $requestFactory;
+
+    public function __construct()
+    {
+        $this->requestFactory = GeneralUtility::makeInstance(RequestFactory::class);
+    }
+
     protected function executeInternal(): ?string
     {
-        /* \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($this->finisherContext);
-        new testCall(); */
+        $formId = $this->parseOption('formId');
+        $formValues = $this->finisherContext->getFormValues();
+        $formRuntime = $this->finisherContext->getFormRuntime();
+        $formDefinition = $formRuntime->getFormDefinition();
+        $successPageUid = (int) $this->parseOption('successPage');
+        $errorPageUid = (int) $this->parseOption('errorPage');
+        $url = self::NL2GO_SUBMIT_URL . $formId;
+
+        $recipient = [];
+        foreach ($formValues as $key => $value) {
+            $element = $formDefinition->getElementByIdentifier($key);
+            $properties = $element?->getProperties();
+            $brevoAttribute = $properties['brevoAttribute'] ?? null;
+
+            if ($brevoAttribute) {
+                $recipient[$brevoAttribute] = $value;
+            }
+        }
+
+        $additionalOptions = [
+            RequestOptions::JSON => [
+                'recipient' => $recipient,
+            ],
+            RequestOptions::HTTP_ERRORS => false,
+            RequestOptions::TIMEOUT => 10,
+        ];
+
+        $response = null;
+        try {
+            $response = $this->requestFactory->request($url, 'POST', $additionalOptions);
+        } catch (Exception $e) {
+            // Nothing to do here, redirect to error page instead
+        }
+
+        $redirectPid = null;
+        if ($response) {
+            $statusCode = $response->getStatusCode();
+            $redirectPid = $statusCode === 201 ? $successPageUid : $errorPageUid;
+        }
+
+        if (!$redirectPid) {
+            $redirectPid = $errorPageUid;
+        }
+
+        $this->finisherContext->cancel();
+
+        $redirectUrl = $this->getTypoScriptFrontendController()->cObj->createUrl([
+            'parameter' => $redirectPid,
+        ]);
+
+        return json_encode(
+            [
+                'redirectUrl' => $redirectUrl,
+                'statusCode' => 303,
+            ],
+            JSON_THROW_ON_ERROR
+        );
     }
 }
